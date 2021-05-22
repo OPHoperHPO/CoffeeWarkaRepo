03477ffc10668ad26ba387ebd5fd5eeeef67e915
Nikita Selin (Anodev) <farvard34@gmail.com>
2021-05-22 13:25:48 +1000

    1

--- CoffeeCoin.sol
+++ CoffeeCoin.sol
@@ -1,35 +1,15 @@
 pragma solidity ^0.8.4;
 // SPDX-License-Identifier: UNLICENSED
-abstract contract Protocol {
-    function name() public virtual  view returns (string memory);
-    function symbol() public virtual view returns (string memory);
-    function totalSupply() public virtual view returns (uint);
-    function balanceOf(address tokenOwner) public virtual view returns (uint balance);
-    function allowance(address tokenOwner, address spender) public virtual view returns (uint remaining);
-    function transfer(address to, uint tokens) public virtual returns (bool success);
-    function approve(address spender, uint tokens) public virtual returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public virtual returns (bool success);
-
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
-
 
 contract CoffeeCoin is Protocol {
     string private token_name;
     string private token_symbol;
     uint8 public decimals;
-
-    uint256 public _totalSupply;
+    uint256 private _totalSupply;
 
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
 
-    /**
-     * Constrctor function
-     *
-     * Initializes contract with initial supply tokens to the creator of the contract
-     */
     constructor() {
         token_name = "CoffeeWarCoin";
         token_symbol = "CWC";
@@ -40,39 +20,39 @@ contract CoffeeCoin is Protocol {
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
     
-    function name() public override view returns (string memory){
+    function name() public view returns (string){
         return token_name;
     }
-    function symbol() public override view returns (string memory) {
+    function symbol() public  view returns (string memory) {
         return token_symbol;
     }
         
-    function totalSupply() public override view returns (uint) {
+    function totalSupply() public view returns (uint) {
         return _totalSupply  - balances[address(0)];
     }
 
-    function balanceOf(address tokenOwner) public  override view returns (uint balance) {
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
         return balances[tokenOwner];
     }
 
-    function allowance(address tokenOwner, address spender) public  override view returns (uint remaining) {
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
 
-    function approve(address spender, uint tokens) public override returns (bool success) {
+    function approve(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
 
-    function transfer(address to, uint tokens) public override returns (bool success) {
+    function transfer(address to, uint tokens) public returns (bool success) {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
 
-    function transferFrom(address from, address to, uint tokens) public override returns (bool success) {
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
         balances[from] = safeSub(balances[from], tokens);
         allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
